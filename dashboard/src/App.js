083fe0d0f17eda72e8669a49ef4743efeb130952
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-17 18:39:07 +0400

    Routes added in the App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { ROUTE_NAMES } from "./Routes";
+
+import {Layout} from "./Pages/Layout";
+import {Home} from "./Pages/Home";
+import {Dashboard} from "./Pages/Dashboard";
+import {Users} from "./Pages/Users";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={ROUTE_NAMES.HOME} element={<Home/>}/>
+          <Route path={ROUTE_NAMES.DASHBOARD} element={<Dashboard/>}/>
+          <Route path={ROUTE_NAMES.USERS} element={<Users/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
